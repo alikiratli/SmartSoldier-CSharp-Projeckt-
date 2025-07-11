f39cc97467a60ffaa39bdab88ac27c6e51072eb4
alikiratli <alikiratlide@gmail.com>
2025-07-11 23:15:02 +0200

    Delete extra directory

--- SmartSoldier/Models/PersonStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartSoldier.Models
-{
-    public class PersonStatus
-    {
-        public string Vorname { get; set; } = string.Empty;
-        public string Nachname { get; set; } = string.Empty;
-        public bool IstVerbunden { get; set; }
-    }
-}
